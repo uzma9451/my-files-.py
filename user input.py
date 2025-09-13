51f3b40db842ca438fd2cae6e4d76e923bdaf82a
uzma9451 <aashi945175@gmail.com>
2025-09-13 14:31:03 +0530

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,14 @@
+# a=int(input ( "enter the  num of a=",))
+# b=int(input( " enter the  num of b=",))
+# a=a+b
+# print("add=",a)
+# #float
+# c= float (input("enter the number of c  =",))
+# d= float(input("enter the number of d=",))
+# e=c+d
+# print("add=",e)
+
+#string
+#
+a=str(input("enter the number=",))
+print(a)
