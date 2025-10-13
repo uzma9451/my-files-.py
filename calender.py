cbabd0be966d33059bfed950093ba8c9ed40983d
uzma9451 <aashi945175@gmail.com>
2025-10-13 14:01:33 +0530

    Create calender.py

---
+++ calender.py
@@ -0,0 +1,39 @@
+m=str(input("enter the month "))
+if(m=="1")or (m=="january"):
+for m in range (1,32):
+  print("its january",m)
+elif(m=="2") or (m=="february"):
+for m in range(1,29) :
+  print("its february",m)
+elif(m=="3") or (m=="march"):
+for m in range (1,32):
+  print("its march",m)
+elif(m=="4") or ( m=="april"):
+for m in range (1,31):
+     print("its april",m)
+elif(m=="5") or  (m=="may"):
+for m in range (1,32):
+    print("its may",m)
+
+elif(m=="6")  or ( m=="june"):
+for m in range ( 1,31):
+   print("its june",m)
+elif(m=="7") or (m=="july"):
+for m in range (1,32):
+   print("its july",m)
+elif(m=="8") or (m=="august"):
+for m in range (1,32):
+   print ("its august",m)
+elif(m=="9") or (m=="september"):
+|for m in range ( 1,31):
+   print("its september",m)
+elif (m=="10") or (m=="october"):
+for m  in range ( 1,32):
+   print(" its october",m)
+elif (m=="11") or (m=="november"):
+for m in range (1,31):
+   print("its november",m)
+elif(m=="12") or (m=="december"):
+for m in range(1,32):
+   print("its december",m)
+  
