3a4ea7913d1cc1937776007af302c71b737f9c6e
uzma9451 <aashi945175@gmail.com>
2025-10-13 13:22:22 +0530

    Add files via upload

---
+++ alphabet,integer,pos,neg,zero.py
@@ -0,0 +1,14 @@
+n=str(input("enter the value for n="))
+x=eval(input("enter the number for x"))
+if (n=="a" or n=="b" or n=="c" or n=="d" or n=="e" or n=="f"or n=="g"or n=="h"or n=="i" or n=="j" or n=="k" or n=="l" or n=="m" or n=="n" or n=="o" or n=="p" or n=="q" or n=="r" or n=="s" or n=="t" or n=="u" or n=="v" or n=="w" or n=="x" or n=="y" or n=="z"):
+    
+    print("its alphabet")
+    if(x>=0 or x<=0):
+        print("its integer")
+elif(x>0):
+    print("its positive")
+elif(x<0):
+    print("its negaive")
+else:
+    print("its zero")
+    
