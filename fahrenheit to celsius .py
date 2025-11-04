98d829eb115df42abdc4043f39f5b90fc1ee78c6
uzma9451 <aashi945175@gmail.com>
2025-11-04 20:06:30 +0530

    Update fahrenheit to celsius .py

--- fahrenheit to celsius .py
+++ fahrenheit to celsius .py
@@ -1,8 +1,9 @@
-'''C=float(input("ENTER TEMP IN CELSIUS:",))
-FAHRENHEIT=(C*1.8)+32
-print("TEMP IN FAHRENHEIT:",FAHRENHEIT)
-
-if FAHRENHEIT > 90 :
-    print(" its a hot day !")'''
-    
+celsius=float(input("enter the num="))
+if( celsius>=0):
+    fahrenheit=(celsius*9.5)+32
+    print("temp in fahrenheit",fahrenheit)
+else:
+      
+    fahrenheit=(celsius*9.5)+32
+    print("temp in fahrenheit( below freezing point):",fahrenheit)
  
